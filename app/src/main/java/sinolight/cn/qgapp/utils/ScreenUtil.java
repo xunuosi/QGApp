d73b305f03b5ce1d346a472361f885eef657ca94
tf_xunuosi <xunuosi19871202@gmail.com>
2017-07-11 17:04:48 +0800

    Join DropMenu

--- app/src/main/java/sinolight/cn/qgapp/utils/ScreenUtil.java
+++ app/src/main/java/sinolight/cn/qgapp/utils/ScreenUtil.java
@@ -1,6 +1,11 @@
 package sinolight.cn.qgapp.utils;
 
+import android.annotation.TargetApi;
 import android.content.Context;
+import android.graphics.Point;
+import android.os.Build;
+import android.view.Display;
+import android.view.WindowManager;
 
 /**
  * Created by admin on 2017/6/18.
@@ -79,4 +84,22 @@ public class ScreenUtil {
         int screenWidth = getScreenWidth(context);
         return px2dip(context, screenWidth);
     }
+
+    /**
+     * 获取屏幕尺寸
+     */
+    @SuppressWarnings("deprecation")
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
+    public static Point getScreenSize(Context context){
+        WindowManager windowManager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        Display display = windowManager.getDefaultDisplay();
+        if(Build.VERSION.SDK_INT < Build.VERSION_CODES.HONEYCOMB_MR2){
+            return new Point(display.getWidth(), display.getHeight());
+        }else{
+            Point point = new Point();
+            display.getSize(point);
+            return point;
+        }
+    }
+
 }
