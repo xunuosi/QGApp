e38a83046a6535c8c2fc822b07dc6cfe1e0f1362
xns—_bj <xunuosi@163.com>
2017-07-21 11:35:25 +0800

    Add a DicDetailActivity

---
+++ app/src/main/java/sinolight/cn/qgapp/views/activity/KDBDicDetailActivity.java
@@ -0,0 +1,39 @@
+package sinolight.cn.qgapp.views.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import sinolight.cn.qgapp.R;
+
+/**
+ * Created by admin on 2017/7/21.
+ * This is activity which is dictionary of knowledge dataBase.
+ */
+
+public class KDBDicDetailActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public int setLayoutId() {
+        return R.layout.activity_kdb_dic_detail;
+    }
+
+    @Override
+    protected void initViews() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void initializeInjector() {
+
+    }
+}
